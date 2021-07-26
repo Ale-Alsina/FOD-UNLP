b4d48099a9b3e0c0b320123ad892d9c1de98541d
Alejandro Alsina <83216208+Ale-Alsina@users.noreply.github.com>
2021-07-26 08:57:49 -0300

    Update TP01_EJ07.pas

--- TP-01/EJ-07/TP01_EJ07.pas
+++ TP-01/EJ-07/TP01_EJ07.pas
@@ -1,26 +1,12 @@
-{ TP01_EJ07
-Realizar un programa que permita:
- a. Crear un archivo binario a partir de la información almacenada en un archivo de texto.
-    El nombre del archivo de texto es: “novelas.txt”
- b. Abrir el archivo binario y permitir la actualización del mismo. Se debe poder agregar
-    una novela y modificar una existente. Las búsquedas se realizan por código de novela.
-NOTA: La información en el archivo de texto consiste en:
-       * código de novela,
-       * nombre,género y
-       * precio de diferentes novelas argentinas.
-      De cada novela se almacena la información en dos líneas en el archivo de texto.
-       * La primera línea contendrá la siguiente información: código novela, precio, y género, y
-       * la segunda línea almacenará el nombre de la novela.
-}
 program TP01_EJ07;
 uses crt, sysutils;
 CONST
    TXT='novelas.txt';
 TYPE
    rLib = record
-      cod: integer; //código de celular,
-      nom: string;  //el nombre(modelo)
-      gen: string;  //descripcion,
+      cod: integer; //código de Novela,
+      nom: string;  //el nombre(titulo)
+      gen: string;  //genero,
       pre: real;    //precio,
       end;
   fLibros= file of rLib;
@@ -37,13 +23,12 @@ begin
   assign(archivo,'novelas.bin'); rewrite(archivo);
   while NOT EOF(data) do begin
     with d do begin
-      //Cada celular se carga leyendo tres líneas del archivo “celulares.txt”.
       readln(data,cod,bco,pre,l.gen);   // 1er linea codigo, precio, genero (ingresa al reg)
       readln(data,l.nom);                   // 2da linea nombre o titulo ingresa al reg)
       //Parceo datos del txt a valores para el reg bin
       l.cod := strToInt(cod);               // convetir a entero valor del codigo leido del txt
       val(pre,l.pre);                       // convetir a real valor del precio leido del txt
-      writeln(l.cod,' | $ ',l.pre:5:2,' | ',l.gen:25,' | ',l.nom);
+      writeln(l.cod,' | $ ',l.pre:5:2,' | ',l.gen:25,' | ',l.nom);// mostrar reg a guardar en bin
     end;
     write(archivo,l); // Escribir el registro dentro del binario
   end;
@@ -67,29 +52,29 @@ procedure actualizar(var archivo:fLibros);
             {4} write('Su nombre : ');           readln(r.nom);
            end;
         1: begin
-                write('Ingrese codigo a modif (actualmente ',r.cod,'): '); readln(r.cod);
+                write('Ingrese codigo a modif (actualmente-> ',r.cod,'): '); readln(r.cod);
            end;
         2: begin
-                write('Ingrese precio a modif (actualmente $',r.pre:5:2,'): '); readln(r.pre);
+                write('Ingrese precio a modif (actualmente-> $',r.pre:5:2,'): '); readln(r.pre);
            end;
         3: begin
-                write('Ingrese genero a modif (actualmente ',r.gen,'): '); readln(r.gen);
+                write('Ingrese genero a modif (actualmente-> ',r.gen,'): '); readln(r.gen);
            end;
         4: begin
-                writeln('Ingrese nombre a modif (actualmente ',r.nom,'): '); readln(r.nom);
+                writeln('Ingrese nombre a modif (actualmente-> ',r.nom,'): '); readln(r.nom);
            end;
     end;
   end;
   procedure modificar(var archivo:fLibros);
     procedure menuModificar(r:rLib; var op:integer);
     begin
-         writeln('Elegir campo del registro novela c/cod "',r.cod,'" a modificar...');
+         writeln('Elegir campo del registro a modificar novela c/cod:',r.cod,' "',r.nom,'"  ...');
          writeln(' 1: Modifica campo "CODIGO". ');
          writeln(' 2: Modifica campo "PRECIO". ');
          writeln(' 3: Modifica campo "GENERO". ');
          writeln(' 4: Modifica campo "NOMBRE". ');
          write('Ingrese opcion: '); readln(op);
-         while (op > 4)and(op < 1 )do begin
+         while (op > 4)and(op > 0 )do begin
            write('ERROR seleccione numero dentro del rango [4 opciones]: '); readln(op);
            end;
     end;
@@ -142,14 +127,6 @@ begin
   until (op='f') or (op='F');
   close(archivo);
 end;
-
-
-
-
-
-
-
-
 
 //###################################### MENU PRINCIPAL ############################################\\
 procedure menuPrincipal(var archivo:fLibros);
